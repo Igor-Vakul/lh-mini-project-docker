afffa22c7c5a881a9e079b55d02a680e5bae3063
Igor-Vakul <vakulinskyigor@gmail.com>
2026-05-19 20:10:50 +0300

    project fixes

--- ansible-slave/entrypoint.sh
+++ ansible-slave/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+chmod 666 /var/run/docker.sock
+
 cp /shared/authorized_keys /home/ansible/.ssh/authorized_keys
 chown ansible:ansible /home/ansible/.ssh/authorized_keys
 chmod 600 /home/ansible/.ssh/authorized_keys
